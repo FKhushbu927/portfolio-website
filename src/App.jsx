c65f68c5a91e9410b70d89f58b44bfeda054908b
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-12-24 20:32:24 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Services from "./components/Services";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      <div className="h-[4000px]"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
